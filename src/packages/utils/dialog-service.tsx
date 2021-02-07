cb6a54cb753bf1df95f859989e51fd6289a67c75
liyinxia <liyinxia@rtmap.com>
2021-02-07 10:10:21 +0800

    打包代码

---
+++ src/packages/utils/dialog-service.tsx
@@ -0,0 +1,28 @@
+// import { DEFAULT_DELAY } from "element-plus/lib/el-infinite-scroll/src"
+
+// enum DialogServiceEditType {
+//     textarea = 'textarea',
+//     input = 'input'
+// }
+
+// interface DialogServiceOption {
+//     editType: DialogServiceEditType,
+//     editReadonly?: boolean,
+//     editorValue?: string | null,
+//     onConfirm: (val?: string | null) => void
+// }
+// const DialogService = (option: DialogServiceOption) => {
+
+// }
+// export const $$dialog = Object.assign(DialogService, {
+//     input: (initValue?: string, option?: DialogServiceOption) => { 
+//         const dfd = dfter<string | null | undefined>()
+//         const opt:DialogServiceOption = option || {editType:DialogServiceEditType.input,onConfirm:dfd.resolve}
+//         DialogService(opt);
+//         return dfd.promise
+//     },
+//     textarea: (initValue?: string, option?: DialogServiceOption) => {
+         
+//      }, 
+
+// })
